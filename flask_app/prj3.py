1d69400dab19c81c8364122ec7e62049bbbb345c
Anwonseok <anwonseok72@gmail.com>
2022-04-21 12:25:53 +0900

    dfsd

--- flask_app/prj3.py
+++ flask_app/prj3.py
@@ -13,33 +13,33 @@ def create_app():
 
    @app.route('/')
    def home():
-      return render_template('blog.html')
+      return render_template('blog.html'),200
 
-   @app.route('/result',methods = ['POST', 'GET'])
-   def result():
-      if request.method == 'POST':
-         result = request.form
+# @app.route('/result',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
 
-         result=result.to_dict(flat=False)
+#       result=result.to_dict(flat=False)
 
 
-      student_card = pd.DataFrame({'감독':result['감독'],
-                                 '제작사':result['제작사'],
-                                 '수입사':result['수입사'],
-                                 '배급사':result['배급사'],
-                                 '개봉일':[int(result['개봉일'][0])],
-                                 '국적':result['국적'],
-                                 '스크린수':[int(result['스크린수'][0])],
-                                 '장르':result['장르'],
-                                 '등급':result['등급']})
-                              
-      X_tests_encoded = encoder.transform(student_card)  
+#    student_card = pd.DataFrame({'감독':result['감독'],
+#                               '제작사':result['제작사'],
+#                               '수입사':result['수입사'],
+#                               '배급사':result['배급사'],
+#                               '개봉일':[int(result['개봉일'][0])],
+#                               '국적':result['국적'],
+#                               '스크린수':[int(result['스크린수'][0])],
+#                               '장르':result['장르'],
+#                               '등급':result['등급']})
+                           
+#    X_tests_encoded = encoder.transform(student_card)  
 
-      result2=boosting.predict(X_tests_encoded)      
+#    result2=boosting.predict(X_tests_encoded)      
 
-      return render_template("cover.html",result = str(round(result2[0])),name=result['영화이름'][0])
+#    return render_template("cover.html",result = str(round(result2[0])),name=result['영화이름'][0])
 
-   if __name__ == '__main__':
-      app.run(debug = True)
+# if __name__ == "__main__":
+#     app.run("0.0.0.0", 80)
 
    return app   
