f9de3d9e103f05f9c2fc909a28e446a410e82feb
Anwonseok <anwonseok72@gmail.com>
2022-04-25 16:19:47 +0900

    first

--- app.py
+++ app.py
@@ -13,29 +13,30 @@ app = Flask(__name__)
 def home():
    return render_template('blog.html'),200
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result',methods = ['POST'])
 def result():
+      
    if request.method == 'POST':
       result = request.form
 
       result=result.to_dict(flat=False)
 
+   
+      student_card = pd.DataFrame({'감독':result['감독'],
+                                 '제작사':result['제작사'],
+                                 '수입사':result['수입사'],
+                                 '배급사':result['배급사'],
+                                 '개봉일':[int(result['개봉일'][0])],
+                                 '국적':result['국적'],
+                                 '스크린수':[int(result['스크린수'][0])],
+                                 '장르':result['장르'],
+                                 '등급':result['등급']})
+                        
+      X_tests_encoded = encoder.transform(student_card)  
 
-   student_card = pd.DataFrame({'감독':result['감독'],
-                              '제작사':result['제작사'],
-                              '수입사':result['수입사'],
-                              '배급사':result['배급사'],
-                              '개봉일':[int(result['개봉일'][0])],
-                              '국적':result['국적'],
-                              '스크린수':[int(result['스크린수'][0])],
-                              '장르':result['장르'],
-                              '등급':result['등급']})
-                           
-   X_tests_encoded = encoder.transform(student_card)  
+      result2=boosting.predict(X_tests_encoded)      
 
-   result2=boosting.predict(X_tests_encoded)      
-
-   return render_template("cover.html",result = str(round(result2[0])),name=result['영화이름'][0])
+      return render_template("cover.html",result = str(round(result2[0])),name=result['영화이름'][0])
 
 if __name__ == "__main__":
     app.run(debug=True)
